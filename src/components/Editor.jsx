e7aeca2be1d1c4267f01b9816e605662cd6395c6
MerakiShubh <shubhamt.champ@gmail.com>
2024-01-12 23:46:38 +0530

    Feature: text will sync in each user

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import Codemirror from "codemirror";
 import "codemirror/mode/javascript/javascript";
 import "codemirror/theme/dracula.css";
@@ -6,20 +6,45 @@ import "codemirror/addon/edit/closetag";
 import "codemirror/addon/edit/closebrackets";
 import "codemirror/lib/codemirror.css";
 
-const Editor = () => {
+const Editor = ({ socketRef, roomId }) => {
+  const editorRef = useRef(null);
   useEffect(() => {
     async function init() {
-      Codemirror.fromTextArea(document.getElementById("realtimeEditor"), {
-        mode: { name: "javascript", json: true },
-        theme: "dracula",
-        autoCloseTags: true,
-        autoCloseBrackets: true,
-        lineNumbers: true,
+      editorRef.current = Codemirror.fromTextArea(
+        document.getElementById("realtimeEditor"),
+        {
+          mode: { name: "javascript", json: true },
+          theme: "dracula",
+          autoCloseTags: true,
+          autoCloseBrackets: true,
+          lineNumbers: true,
+        }
+      );
+      editorRef.current.on("change", (instance, changes) => {
+        // console.log(changes);
+        const { origin } = changes;
+        const code = instance.getValue();
+        if (user !== "setValue") {
+          socketRef.current.emit("code-change", {
+            roomId,
+            code,
+          });
+        }
       });
     }
 
     init();
   }, []);
+
+  useEffect(() => {
+    if (socketRef.current) {
+      socketRef.current.on("code-change", ({ code }) => {
+        if (code !== null) {
+          editorRef.current.setValue(code);
+        }
+      });
+    }
+  }, [socketRef.current]);
 
   return <textarea id="realtimeEditor"></textarea>;
 };
